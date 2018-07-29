3fa792bfc9c3a7832bafd0acfe51c343912379e3
Dan Barthell <dbarthell@Dans-MacBook-Pro.local>
2018-07-28 23:06:11 -0500

    temp

--- server.js
+++ server.js
@@ -23,13 +23,13 @@ mongoose.connect(MONGODB_URI);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build/public"));
+  app.use(express.static("client/public"));
 }
 
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile("/Users/dbarthell/Knack/create-react-express/client/public/index.html");
 });
 
 app.listen(PORT, function() {
