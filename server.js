9fe29d3c3988d9d5b6166083ea5b212be894bfbe
Dan Barthell <dbarthell@Dans-MacBook-Pro.local>
2018-07-28 20:41:08 -0500

    added mongo config

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
